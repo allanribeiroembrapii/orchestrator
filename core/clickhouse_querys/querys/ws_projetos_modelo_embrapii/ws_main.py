1764e50587a1ac0f7e017b65ebc5f3c0b5dd92f6
Allan Ribeiro <akosr91@gmail.com>
2025-04-02 15:41:39 -0300

    Alteração de srinfo_project_contract.py para srinfo_project.py. Ajuste no gitignore para ignorar arquivos csv e xlsx.

--- core/clickhouse_querys/querys/ws_projetos_modelo_embrapii/ws_main.py
+++ core/clickhouse_querys/querys/ws_projetos_modelo_embrapii/ws_main.py
@@ -1,5 +1,5 @@
 import inspect
-from querys.ws_projetos_modelo_embrapii.srinfo_project_contract import srinfo_project_contract
+from querys.ws_projetos_modelo_embrapii.srinfo_project import srinfo_project
 from querys.ws_projetos_modelo_embrapii.srinfo_prospect import srinfo_prospect
 from querys.ws_projetos_modelo_embrapii.srinfo_negotiation import srinfo_negotiation
 
@@ -7,9 +7,9 @@ def ws_projetos_modelo_embrapii():
     print("🟡 " + inspect.currentframe().f_code.co_name)
     try:
         #Querys
-        # srinfo_project_contract()
         srinfo_prospect()
         srinfo_negotiation()
+        srinfo_project()
 
         print("🟢 " + inspect.currentframe().f_code.co_name)
     except Exception as e:
