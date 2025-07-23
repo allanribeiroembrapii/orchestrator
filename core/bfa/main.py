d063ab12167ef96fb40202d8b358d7f230f4aba9
milena-sgoncalves <milena.goncalves@embrapii.org.br>
2025-07-23 16:19:30 -0300

    Inclusão do script para buscar e tratar planilhas do BFA no Sharepoint da GERIN.

---
+++ core/bfa/main.py
@@ -0,0 +1,35 @@
+from scripts.buscar_arquivos_sharepoint import buscar_arquivos_sharepoint
+from scripts.gerar_planilhas import gerar_planilhas, processar_planilhas
+from office365_api.upload_files import upload_files
+from scripts.zipar_arquivos import zipar_arquivos
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ROOT = os.getenv('ROOT_BFA')
+STEP3 = os.path.abspath(os.path.join(ROOT, 'data', 'step_3_data_processed'))
+BACKUP = os.path.abspath(os.path.join(ROOT, 'data', 'backup'))
+COPY = os.path.abspath(os.path.join(ROOT, 'data', 'copy'))
+
+def main():
+    # Buscando arquivo do BFA no SharePoint
+    print("Passo 1/4: Buscando arquivo do BFA no SharePoint...")
+    buscar_arquivos_sharepoint()
+    zipar_arquivos(COPY, BACKUP)
+
+    # Gerando planilhas a partir do arquivo do BFA
+    print("Passo 2/4: Gerando planilhas a partir do arquivo do BFA...")
+    gerar_planilhas()
+
+    # Processando as planilhas geradas
+    print("Passo 3/4: Processando as planilhas geradas...")
+    processar_planilhas()
+
+    # Levando as planilhas processadas para o SharePoint
+    print("Passo 4/4: Enviando planilhas processadas para o SharePoint...")
+    upload_files(STEP3, 'dw_pii')
+    upload_files(BACKUP, 'DWPII_backup')
+
+
+if __name__ == "__main__":
+    main()
---
+++ core/bfa/office365_api/upload_files.py
@@ -0,0 +1,41 @@
+import re
+import os
+from pathlib import PurePath
+from dotenv import load_dotenv
+from office365_api.office365_api import SharePoint
+
+load_dotenv()
+SHAREPOINT_SITE = os.getenv('sharepoint_url_site')
+SHAREPOINT_SITE_NAME = os.getenv('sharepoint_site_name')
+SHAREPOINT_DOC = os.getenv('sharepoint_doc_library')
+
+
+def upload_files(pasta_arquivos, destino, keyword=None, arquivo_especifico=None):
+    if arquivo_especifico:
+        file_list = [[os.path.basename(arquivo_especifico), arquivo_especifico]]
+    else:
+        file_list = get_list_of_files(pasta_arquivos)
+    
+    for file in file_list:
+        if keyword is None or keyword == 'None' or re.search(keyword, file[0]):
+            file_content = get_file_content(file[1])
+            SharePoint(
+                site_url = SHAREPOINT_SITE,
+                site_name = SHAREPOINT_SITE_NAME,
+                doc_library = SHAREPOINT_DOC
+            ).upload_file(file[0], destino, file_content)
+
+
+def get_list_of_files(folder):
+    file_list = []
+    folder_item_list = os.listdir(folder)
+    for item in folder_item_list:
+        item_full_path = PurePath(folder, item)
+        if os.path.isfile(item_full_path):
+            file_list.append([item, item_full_path])
+    return file_list
+
+# read files and return the content of files
+def get_file_content(file_path):
+    with open(file_path, 'rb') as f:
+        return f.read()
